1ad33cad916a50e0e34886cf375c39b06dd1baab
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-27 02:37:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import MoviesPage from '../pages/MoviesPage';
+import Layout from './Layout/Layout';
+import FilmDetails from './FilmDetails/FilmDetails';
+import Cast from '../components/Cast/Cast';
+import Rewievs from '../components/Rewievs/Rewievs';
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="movies" element={<MoviesPage />} />
+        <Route path="movies/:filmId" element={<FilmDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="rewievs" element={<Rewievs />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
+export default App;
