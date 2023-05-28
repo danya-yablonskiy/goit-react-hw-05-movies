5e0d0fdb0f3019adc103064a42495d6c4b7bd0c7
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-28 12:55:59 +0300

    Create Header.styled.js

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+
+export const List = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+export const ListItem = styled.li`
+margin-left: 40px;
+`
+export const StyledLink = styled(NavLink)`
+  text-decoration: none;
+  color: inherit;
+  &.active {
+    color: orange;
+  }
+`;
+
+export const Container = styled.div`
+  border-bottom: 1px solid black;
+`;
