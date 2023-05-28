99fc886285e9357ccecb6573e65ceac94e0f9480
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-28 14:02:55 +0300

    Update FilmDetails.jsx

--- src/components/FilmDetails/FilmDetails.jsx
+++ src/components/FilmDetails/FilmDetails.jsx
@@ -1,7 +1,7 @@
 import { Link, useParams, Outlet, useLocation } from 'react-router-dom';
 import getOneFilm from 'components/API/getOneFilm';
 import { Suspense, useEffect, useRef, useState } from 'react';
-
+import { Container, Wrapper, Inf } from './FilmDetails.styled';
 const FilmDetails = () => {
   const [details, setDetails] = useState([]);
   const { filmId } = useParams();
@@ -16,32 +16,36 @@ const FilmDetails = () => {
   return (
     <div>
       <Link to={backLinkHref.current}>Back</Link>
-      <div>
-        <img
-          src={
-            details.poster_path &&
-            `https://image.tmdb.org/t/p/w500${details.poster_path}`
-          }
-          alt={`Poster for ${details.title}`}
-          width="300"
-          height="400"
-        />
-        <h1>{details.title}</h1>
-        <p>User score: {(details.vote_average * 10).toFixed(0)}%</p>
-        <h2>Overview</h2>
-        <p> {details.overview}</p>
-        <h3>Genres</h3>
-        <ul>
-          {details.genres &&
-            details.genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
-      </div>
-      <div>
+      <Container>
+        <div>
+          <img
+            src={
+              details.poster_path &&
+              `https://image.tmdb.org/t/p/w500${details.poster_path}`
+            }
+            alt={`Poster for ${details.title}`}
+            width="300"
+            height="400"
+          />
+        </div>
+        <Wrapper>
+          <h1>{details.title}</h1>
+          <p>User score: {(details.vote_average * 10).toFixed(0)}%</p>
+          <h2>Overview</h2>
+          <p> {details.overview}</p>
+          <h3>Genres</h3>
+          <ul>
+            {details.genres &&
+              details.genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+          </ul>
+        </Wrapper>
+      </Container>
+      <Inf>
         <p>Additional infotmation</p>
         <Link to={'cast'}>Cast</Link>
         <br />
         <Link to={'rewievs'}>Rewievs</Link>
-      </div>
+      </Inf>
       <Suspense>
         <Outlet />
       </Suspense>
