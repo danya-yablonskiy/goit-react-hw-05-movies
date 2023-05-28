3bfe9d40f7ed2f527ba4d5930a995162e4f1e66c
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-28 12:56:01 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,14 @@
 import Header from 'components/Header/Header';
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Suspense>
+        <Outlet />
+      </Suspense>
     </>
   );
 };
