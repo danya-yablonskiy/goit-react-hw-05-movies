c7962aada3dd0a3462a7c964255dec4320e2f224
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-28 12:55:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
-import MoviesPage from '../pages/MoviesPage';
+// import MoviesPage from '../pages/MoviesPage';
 import Layout from './Layout/Layout';
 import FilmDetails from './FilmDetails/FilmDetails';
 import Cast from '../components/Cast/Cast';
 import Rewievs from '../components/Rewievs/Rewievs';
+
+const MoviesPage = lazy(() => import('../pages/MoviesPage'));
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="movies" element={<MoviesPage />} />
+        <Route
+          path="movies"
+          element={
+            <Suspense>
+              <MoviesPage />
+            </Suspense>
+          }
+        />
         <Route path="movies/:filmId" element={<FilmDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="rewievs" element={<Rewievs />} />
