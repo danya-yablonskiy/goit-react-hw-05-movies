bebdb25fecb8a1355e97cda045e3618e8aa9af94
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-28 14:02:57 +0300

    Create FilmDetails.styled.js

---
+++ src/components/FilmDetails/FilmDetails.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const Wrapper = styled.div`
+margin-left: 20px;
+`
+
+export const Inf = styled.div`
+border-top: 1px solid black;
+border-bottom: 1px solid black;
+padding: 10px;
+`
